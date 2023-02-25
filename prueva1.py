12dc279163cd29aca24fcfa83907995ee2d3f0ad
senatinoprohacker <1475937@senati.pe>
2023-02-25 09:12:05 -0500

    hola

---
+++ prueva1.py
@@ -0,0 +1,13 @@
+# realiza una suma y multiplicasión con sus respectivas indicacions:
+
+a=12
+b=52
+print("la suma es",a+b)
+
+# realizando una multipliasión:
+a=5
+b=6
+print("la repuesta es",a*b)
+ 
+#saludando al prosor
+print("hi teacher")
